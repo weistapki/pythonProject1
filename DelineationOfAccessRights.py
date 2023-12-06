aeca0d60dff73e8e0529c3ac76e8dc08c5d75af7
weistapki <weistapki@gmail.com>
2023-12-06 18:30:19 +0100

    added delineation of access rights

---
+++ DelineationOfAccessRights.py
@@ -0,0 +1,48 @@
+class User:
+    login = "Guest"
+
+    def greet(self):
+        print(f"Hi, my login is {self.login}")
+
+
+class AuthenticatedUser(User):
+    password = "password"
+
+    def authenticate(self, user_password):
+        return user_password == self.password
+
+
+class AccessObject:
+    content = "*"
+    owner = None
+    name = ""
+
+    def change_owner(self, old_owner_password, new_owner):
+        if self.owner is None:
+            print("Object owner is not set.")
+        elif self.owner.authenticate(old_owner_password):
+            print(f"The ownership changing of '{self.name}' was successful! New owner is {new_owner.login}.")
+            self.owner = new_owner
+        else:
+            print(f"Unauthorised attempt of '{self.name}' owner changing detected!")
+
+
+if __name__ == '__main__':
+    alice = AuthenticatedUser()
+    alice.login = "Alice"
+    bob = AuthenticatedUser()
+    bob.login = "Bob"
+    bob.password = "uNc1e"
+
+    log = AccessObject()
+    log.owner = alice
+    log.name = "Computer logs"
+    log.content = "There is no entries yet"
+
+    security_policy = AccessObject()
+    security_policy.owner = bob
+    security_policy.name = "Enterprise security policy"
+    security_policy.content = "Only authorized users may access objects"
+
+    log.change_owner(bob.password, bob)
+    security_policy.change_owner(bob.password, alice)
